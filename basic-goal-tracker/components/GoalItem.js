c56afbcb5dd1ffd4f6a97e96417c17d5afe6395e
Jaycob Duffel <48539549+JaycobDuffel@users.noreply.github.com>
2022-12-08 12:13:11 -0800

    Add feedback when pressing

--- basic-goal-tracker/components/GoalItem.js
+++ basic-goal-tracker/components/GoalItem.js
@@ -3,22 +3,26 @@ import { StyleSheet, Text, Pressable, View } from "react-native";
 export default function GoalItem({ item, onDeleteItem }) {
 
   return (
-    <Pressable onPress={onDeleteItem.bind(this, item.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable android_ripple={{color: '#dddddd'}} style={({pressed}) => pressed && styles.pressedItem} onPress={onDeleteItem.bind(this, item.id)}>
         <Text style={styles.goalText}>{item.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   goalItem: {
-    padding: 8,
     marginVertical: 8,
     backgroundColor: "rebeccapurple",
     borderRadius: 5,
   },
+  pressedItem: {
+    backgroundColor: "red",
+    opacity: 0.7,
+  },
   goalText: {
     color: "#ffd800",
+    padding: 8,
   },
 });
